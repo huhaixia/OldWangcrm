9a76a262a8c0887db8e24f84cea7461b69569eff
王胜敏 <wsm.hn@qq.com>
2016-07-02 17:36:36 +0800

    0702创建工程

---
+++ OldWangCrm/src/main/java/com/oldwang/oldwangcrm/model/Picture.java
@@ -0,0 +1,58 @@
+package com.oldwang.oldwangcrm.model;
+/**
+ * ͼƬ
+ * 
+ * @author Administrator
+ * 
+ */
+public class Picture {
+	/**
+	 * ͼƬid
+	 */
+	private int id;
+	/**
+	 * ͼƬ��ַ
+	 */
+	private String src;
+	/**
+	 * ��Դ
+	 */
+	private String area;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the src
+	 */
+	public String getSrc() {
+		return src;
+	}
+	/**
+	 * @param src the src to set
+	 */
+	public void setSrc(String src) {
+		this.src = src;
+	}
+	/**
+	 * @return the area
+	 */
+	public String getArea() {
+		return area;
+	}
+	/**
+	 * @param area the area to set
+	 */
+	public void setArea(String area) {
+		this.area = area;
+	}
+	
+}
